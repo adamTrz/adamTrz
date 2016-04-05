857cbb9dcfd28729a54c07d0f9c04a15041d67c9
adamTrz <trzcinski.adam@gmail.com>
2016-04-05 18:28:48 +0200

    another day :) - styling cotact and portfolio sections; added images

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,61 +1,87 @@
-var app = angular.module('app', [])
+(function(){
+  angular.module('app', ['ngAnimate'])
 
-app.controller('mainCtrl',function () {
-  this.activeSection = 1
-  this.setSection = ((section)=>{
-    this.activeSection = section
+  .controller('mainCtrl',function () {
+    this.activeSection = 1
+    this.setSection = ((section) => {
+      this.activeSection = section
+    })
+    this.sectionSelected = ((section) => {
+      return this.activeSection === section
+    })
   })
-  this.sectionSelected = ((section) => {
-    return this.activeSection === section
+
+  .controller('navCtrl', function(){
+    this.showMenu = 0
+    // this.fun(){}
+    this.showAnchors = (() => {
+      this.showMenu = this.showMenu ? 0 : 1
+    })
   })
-})
 
-app.controller('navCtrl', function(){
-  this.showMenu = 0
-  this.showAnchors = (() => {
-    this.showMenu = this.showMenu ? 0 : 1
+  .controller('portfolioCtrl', function(){
+    this.gallery = portfolioGallery
   })
-})
 
-app.controller('portfolioCtrl', function(){
-  this.gallery = portfolioGallery
-})
-
-app.controller('contactCtrl', function(){
-  this.mail = {}
-  this.formSubmit = ((mail,form) => {
-    console.log('thanks');
+  .controller('contactCtrl', function(){
     this.mail = {}
-    form.$setPristine()
-    console.log(form);
-  }
-  )
-})
+    this.socials = contactList
+    this.formSubmit = ((mail,form) => {
+      console.log('thanks');
+      this.mail = {}
+      form.$setPristine()
+      console.log(form);
+    }
+    )
+  })
 
-let portfolioGallery = [
-    { size: 'thin',
-      url: 'http://fillmurray.com/g/600/600',
-      blob: 'bananas',
-      gitLink: 'https://github.com/adamTrz',
-      wwwLink: '//google.pl'},
-    { size: 'wide',
-      url: 'http://fillmurray.com/g/800/1200',
-      blob: 'bananas',
-      gitLink: 'https://github.com/adamTrz',
-      wwwLink: '//google.pl'},
-    { size: 'wide',
-      url: 'http://fillmurray.com/g/800/800',
-      blob: 'bananas',
-      gitLink: 'https://github.com/adamTrz',
-      wwwLink: '//google.pl'},
-    { size: 'thin',
-      url: 'http://fillmurray.com/g/1000/800',
-      blob: 'bananas',
-      gitLink: 'https://github.com/adamTrz',
-      wwwLink: '//google.pl'},
-    { size: 'thin',
-      url: 'http://fillmurray.com/g/1200/1000',
-      blob: 'bananas',
-      gitLink: 'https://github.com/adamTrz',
-      wwwLink: '//google.pl'}
+
+  let contactList = [
+    { url:    'https://github.com/adamTrz',
+      title:  'GitHub',
+      icon:   'fa-git',
+    },
+    { url:    'http://codepen.io/adamTrz/',
+      title:  'CodePen',
+      icon:   'fa-codepen',
+    },
+    { url:    'https://www.linkedin.com/in/adamtrz',
+      title:  'LinkedIn',
+      icon:   'fa-linkedin',
+    }
   ]
+
+  let portfolioGallery = [
+      { size:     'four',
+        url:      'http://fillmurray.com/g/600/600',
+        blob:     'Portfolio - adamTrz',
+        gitLink:  'https://github.com/adamTrz',
+        wwwLink:  '//google.pl'},
+      { size:     'eight',
+        url:      './assets/img/y.jpg',
+        blob:     'Magic of Y',
+        gitLink:  'https://github.com/adamTrz',
+        wwwLink:  '//google.pl'},
+      { size:     'twelve',
+        url:      './assets/img/black-bird.jpg',
+        blob:     'Black Bird Store',
+        gitLink:  'https://github.com/adamTrz',
+        wwwLink:  '//google.pl'},
+      { size:     'twelve',
+        url:      './assets/img/sentiOne.jpg',
+        blob:     'Template for SentiOne',
+        gitLink:  'https://github.com/adamTrz',
+        wwwLink:  '//google.pl'},
+      { size:     'six',
+        url:      './assets/img/deska.png',
+        blob:     'Template for EasySend',
+        gitLink:  'https://github.com/adamTrz',
+        wwwLink:  '//google.pl'},
+      { size:     'six',
+        url:      './assets/img/kafelki.jpg',
+        blob:     'Template for Green Beam',
+        gitLink:  'https://github.com/adamTrz',
+        wwwLink:  '//google.pl'},
+    ]
+
+})()
