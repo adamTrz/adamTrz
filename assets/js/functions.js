94443aeb2c33fb1677d7f298dd20841e783f1099
adamTrz <trzcinski.adam@gmail.com>
2016-04-08 00:42:18 +0200

    more ankgular work - added couple directives, fixed ui-routes; move json data to external files and fetch em inside of resolves; added scroll magic into portolio directives; some animations and additional styling

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,75 +1,117 @@
 (function(){
   'use strict'
-  let app =  angular.module('app', ['ngAnimate','ngRoute'])
+  let app =  angular.module('app', ['ngAnimate','ui.router'])
+
+  //hack from http://stackoverflow.com/questions/17497006/use-http-inside-custom-provider-in-app-config-angular-js/21536845#21536845
+  // allow to inject $http into configuration resolve function
+  let initInjector = angular.injector(['ng']);
+  let $http = initInjector.get('$http');
 
 
-  app.config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-      .when('/contact' , {
-        templateUrl:  './pages/contact.html',
-        controller:   'contactCtrl',
-        controllerAs: 'contact',
+  app.run(['$rootScope', '$state', '$stateParams', function ($rootScope, $state,  $stateParams) {
+    // It's very handy to add references to $state and $stateParams to the $rootScope so that you can access them from any scope within your applications.For example, <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li> to active whenever 'contacts.list' or one of its decendents is active.
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
+    $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
+      console.log(error);
+    })
+  }])
+
+  app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider
+      .otherwise('/about')
+    $stateProvider
+      .state('about', {
+        url:            '/about',
+        templateUrl:    './pages/about.html',
+        controller:     'aboutCtrl',
+        controllerAs:   'about'
       })
-      .when('/portfolio' , {
-        templateUrl:  './pages/portfolio.html',
-        controller:   'portfolioCtrl',
-        controllerAs: 'portfolio',
+      .state('contact', {
+        url:            '/contact',
+        templateUrl:    './pages/contact.html',
+        controller:     'contactCtrl',
+        controllerAs:   'contact',
+        resolve:        {
+          socialLinks:  ['$http', (($htpp) => {
+            return $http.get('./assets/api/socialLinks.json')
+              .then(function(response) {
+                return response.data})
+          })]
+        }
       })
-      .when('/about' , {
-        templateUrl:  './pages/about.html',
-        controller:   'aboutCtrl',
-        controllerAs: 'about',
-      })
-      .when('/' , {
-        templateUrl:  './pages/about.html',
-        controller:   'aboutCtrl',
-        controllerAs: 'about',
-      })
-      .otherwise({
-        templateUrl:  './pages/404.html'
+      .state('portfolio', {
+        url:            '/portfolio',
+        templateUrl:    './pages/portfolio.html',
+        controller:     'portfolioCtrl',
+        controllerAs:   'portfolio',
+        resolve:        {
+          pictures:  ['$http', (($htpp) => {
+            return $http.get('./assets/api/portfolioGallery.json')
+              .then(function(response) {
+                return response.data})
+          })],
+          icons:  ['$http', (($htpp) => {
+            return $http.get('./assets/api/icons.json')
+              .then(function(response) {
+                return response.data})
+          })]
+        }
       })
   }])
 
   app.directive('navigation',function() {
     return {
-      restrict: 'E',
-      templateUrl: './pages/nav.html',
-      controller: 'navCtrl',
-      controllerAs: 'nav'
+      restrict:       'E',
+      templateUrl:    './pages/nav.html',
+      controller:     'navCtrl',
+      controllerAs:   'nav'
+    }
+  })
+
+  let controller = new ScrollMagic.Controller()
+  app.directive('portfolioItem', function() {
+    return {
+      restrict:       'A',
+      templateUrl:     './pages/portfolio-item.html',
+      link: function(scope, elem, attrs) {
+        console.log(elem[0]);
+        var scene = new ScrollMagic.Scene({
+            triggerElement: elem[0],
+            offset: -100
+        })
+      .setClassToggle(elem[0], 'is-active')
+      .addTo(controller);
+      // console.log(scene);
+      }
     }
   })
 
   app.controller('aboutCtrl',function(){
   })
-  // app.run(function() {
-  //
-  //   })
-
 
   app.controller('navCtrl', function(){
-    this.showMenu = 0
-    this.activePane = 2
+    this.showMenu = 1
     this.showAnchors = (() => {
       this.showMenu = this.showMenu ? 0 : 1
     })
-    this.setActive = ((num) => {
-      this.activePane = num
-    })
   })
 
-  app.controller('portfolioCtrl', function(){
-    this.gallery = portfolioGallery
-    this.pairs = iconPairs
+  app.controller('portfolioCtrl', ['pictures','icons', function(pictures,icons){
+    this.gallery = pictures
+    // console.log(this.gallery2);
+    // this.gallery = portfolioGallery
+    this.icons = icons
     this.iconClass = ((task) => {
-      let classObj =  this.pairs.filter((obj) => {
+      let classObj =  this.icons.filter((obj) => {
         return obj.task === task
       })
       return classObj[0].icon
     })
-  })
-  app.controller('contactCtrl', function(){
+  }])
+  app.controller('contactCtrl', ['socialLinks', function(socialLinks){
     this.mail = {}
-    this.socials = contactList
+    this.socials = socialLinks
     this.formSubmit = ((mail,form) => {
       console.log('thanks');
       this.mail = {}
@@ -77,91 +119,34 @@
       console.log(form);
     }
     )
-  })
+  }])
 
-  const contactList = [
-    { url:    'https://github.com/adamTrz',
-      title:  'GitHub',
-      icon:   'fa-git',
-    },
-    { url:    'http://codepen.io/adamTrz/',
-      title:  'CodePen',
-      icon:   'fa-codepen',
-    },
-    { url:    'https://www.linkedin.com/in/adamtrz',
-      title:  'LinkedIn',
-      icon:   'fa-linkedin',
-    }
-  ]
-  const portfolioGallery = [
-      { url:      'http://fillmurray.com/g/600/300',
-        title:    'Portfolio - adamTrz.',
-        tasks:     ['design', 'layout', 'code','animations'],
-        techs:     ['Jade','Sass','Gulp','Angular','Skeleton', 'ScrollMagic'],
-        gitLink:  'https://github.com/adamTrz',
-        wwwLink:  '//google.pl'},
-      { url:      './assets/img/y.jpg',
-        title:     'Magic of Y - contact page for Polish acclaimed illusionist "Y".',
-        tasks:     ['design', 'layout', 'code','animations'],
-        techs:     ['Jade','Sass','Gulp','Gsap','jQuery'],
-        gitLink:  'https://github.com/adamTrz',
-        wwwLink:  '//google.pl'},
-      { url:      './assets/img/black-bird.jpg',
-        title:     'Black Bird Store - a simple paralax technique example.',
-        tasks:     ['code', 'animations'],
-        techs:     ['Jade','Sass','jQuery'],
-        gitLink:  'https://github.com/adamTrz',
-        wwwLink:  '//google.pl'},
-      { url:      './assets/img/sentiOne.jpg',
-        title:     'Responsive template for SentiOne.',
-        tasks:     ['code', 'animations'],
-        techs:     ['Jade','Sass','Gulp','Skeleton'],
-        gitLink:  'https://github.com/adamTrz',
-        wwwLink:  '//google.pl'},
-      { url:      './assets/img/deska.png',
-        title:     'Responsive template for EasySend',
-        tasks:     ['code', 'animations'],
-        techs:     ['Jade','Sass','Gulp','jQuery'],
-        gitLink:  'https://github.com/adamTrz',
-        wwwLink:  '//google.pl'},
-      { url:      './assets/img/kafelki.jpg',
-        title:     'Responsive template for Green Beam',
-        tasks:     ['code', 'animations'],
-        techs:     ['Jade','Sass','Gulp','Bootstrap'],
-        gitLink:  'https://github.com/adamTrz',
-        wwwLink:  '//google.pl'},
-    ]
-  const iconPairs = [
-    {task:  'design',     icon: 'lightbulb_outline'},
-    {task:  'layout',     icon: 'line_style'},
-    {task:  'code',       icon: 'code'},
-    {task:  'animations', icon: 'crop_rotate'}
-  ]
 
 //////////////////////////////////////////////////
 })()
 
 $(function(){
 
+
   // Init ScrollMagic
-  var controller = new ScrollMagic.Controller();
+  // var controller = new ScrollMagic.Controller();
   // get all triggers - headers of all 3 slides
   // var $headers = $('portfolio #portfolio article')
   // console.log($headers);
   // var headers = jQuery.makeArray($headers)
-  var headers = ['slide01','slide02','slide03','slide04','slide05','slide06',]
+  // var headers = ['slide01','slide02','slide03','slide04','slide05','slide06',]
   // console.log(headers);
   // SCENE 1
   // create scenes for each of the headers
-  headers.forEach(function (header, index) {
-    // number for highlighting scenes
-    var num = index+1;
-      // make scene
-      var headerScene = new ScrollMagic.Scene({
-          triggerElement: header,
-          offset: -100
-      })
-      .setClassToggle('#slide0'+num, 'is-active')
-      .addTo(controller);
-  });
+  // headers.forEach(function (header, index) {
+  //   // number for highlighting scenes
+  //   var num = index+1;
+  //     // make scene
+  //     var headerScene = new ScrollMagic.Scene({
+  //         triggerElement: header,
+  //         offset: -100
+  //     })
+  //     .setClassToggle('#slide0'+num, 'is-active')
+  //     .addTo(controller);
+  // });
 });
