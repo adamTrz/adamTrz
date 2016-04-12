681a71fabd781ee219003d761df47eeabe0ba740
adamTrz <trzcinski.adam@gmail.com>
2016-04-12 16:45:52 +0200

    cleaning up the mess from bad merge...

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,14 +1,5 @@
 (function(){
   'use strict'
-<<<<<<< HEAD
-  let app =  angular.module('app', ['ngAnimate','ui.router'])
-
-  //hack from http://stackoverflow.com/questions/17497006/use-http-inside-custom-provider-in-app-config-angular-js/21536845#21536845
-  // allow to inject $http into configuration resolve function
-  let initInjector = angular.injector(['ng']);
-  let $http = initInjector.get('$http');
-
-=======
 ///////////////////////////
 // APP DECLARATION AND CONFIG:
 ///////////////////////////
@@ -19,7 +10,6 @@
   // allow to inject $http into configuration resolve function
   let initInjector = angular.injector(['ng']);
   let $http = initInjector.get('$http');
->>>>>>> gh-pages
 
   app.run(['$rootScope', '$state', '$stateParams', function ($rootScope, $state,  $stateParams) {
     // It's very handy to add references to $state and $stateParams to the $rootScope so that you can access them from any scope within your applications.For example, <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li> to active whenever 'contacts.list' or one of its decendents is active.
@@ -89,38 +79,17 @@
       controllerAs:   'nav'
     }
   })
-<<<<<<< HEAD
-
-  let controller = new ScrollMagic.Controller()
-=======
   app.directive('foot', function() {
     return {
       restrict:       'E',
       templateUrl:    './pages/foot.html'
     }
   })
->>>>>>> gh-pages
   app.directive('portfolioItem', function() {
     return {
       restrict:       'A',
       templateUrl:     './pages/portfolio-item.html',
       link: function(scope, elem, attrs) {
-<<<<<<< HEAD
-        console.log(elem[0]);
-        var scene = new ScrollMagic.Scene({
-            triggerElement: elem[0],
-            offset: -100
-        })
-      .setClassToggle(elem[0], 'is-active')
-      .addTo(controller);
-      // console.log(scene);
-      }
-    }
-  })
-
-  app.controller('aboutCtrl',function(){
-  })
-=======
         var scene = new ScrollMagic.Scene({
             triggerElement: elem[0],
             offset: -200
@@ -147,7 +116,6 @@
     console.log(this.skillset)
 
   }])
->>>>>>> gh-pages
 
   app.controller('navCtrl', function(){
     this.showMenu = 1
@@ -167,10 +135,7 @@
       })
       return classObj[0].icon
     })
-<<<<<<< HEAD
-=======
 
->>>>>>> gh-pages
   }])
   app.controller('contactCtrl', ['socialLinks', function(socialLinks){
     this.mail = {}
@@ -183,38 +148,5 @@
     }
     )
   }])
-<<<<<<< HEAD
-
-=======
->>>>>>> gh-pages
 
 })()
-<<<<<<< HEAD
-
-$(function(){
-
-
-  // Init ScrollMagic
-  // var controller = new ScrollMagic.Controller();
-  // get all triggers - headers of all 3 slides
-  // var $headers = $('portfolio #portfolio article')
-  // console.log($headers);
-  // var headers = jQuery.makeArray($headers)
-  // var headers = ['slide01','slide02','slide03','slide04','slide05','slide06',]
-  // console.log(headers);
-  // SCENE 1
-  // create scenes for each of the headers
-  // headers.forEach(function (header, index) {
-  //   // number for highlighting scenes
-  //   var num = index+1;
-  //     // make scene
-  //     var headerScene = new ScrollMagic.Scene({
-  //         triggerElement: header,
-  //         offset: -100
-  //     })
-  //     .setClassToggle('#slide0'+num, 'is-active')
-  //     .addTo(controller);
-  // });
-});
-=======
->>>>>>> gh-pages
